b474efa2f85f1e1171e53f47a95b1d0ae285fe08
Oliwier Olszewski <oliwierolszewski@proton.me>
2026-04-10 17:59:37 +0200

    Generete declarations of getters and setters for class.

--- Class.cpp
+++ Class.cpp
@@ -4,3 +4,104 @@
 
 #include "Class.h"
 
+Class::Class(const int class_id, Teacher * const match_teacher, Teacher * const english_teacher,
+    Teacher * const polish_teacher, Teacher * const history_teacher, Teacher * const biology_teacher,
+    Teacher * const physics_teacher, Teacher * const chemistry_teacher, Teacher * const geography_teacher,
+    Teacher * const computer_science_teacher, Teacher * const physical_education_teacher,
+    Teacher * const super_vising_teacher): classId(class_id),
+                                           matchTeacher(match_teacher),
+                                           englishTeacher(english_teacher),
+                                           polishTeacher(polish_teacher),
+                                           historyTeacher(history_teacher),
+                                           biologyTeacher(biology_teacher),
+                                           physicsTeacher(physics_teacher),
+                                           chemistryTeacher(chemistry_teacher),
+                                           geographyTeacher(geography_teacher),
+                                           computerScienceTeacher(computer_science_teacher),
+                                           physicalEducationTeacher(physical_education_teacher),
+                                           superVisingTeacher(super_vising_teacher) {
+}
+
+void Class::set_match_teacher(Teacher * const match_teacher) {
+    matchTeacher = match_teacher;
+}
+
+Teacher * Class::get_english_teacher() const {
+    return englishTeacher;
+}
+
+void Class::set_english_teacher(Teacher * const english_teacher) {
+    englishTeacher = english_teacher;
+}
+
+Teacher * Class::get_polish_teacher() const {
+    return polishTeacher;
+}
+
+void Class::set_polish_teacher(Teacher * const polish_teacher) {
+    polishTeacher = polish_teacher;
+}
+
+Teacher * Class::get_history_teacher() const {
+    return historyTeacher;
+}
+
+void Class::set_history_teacher(Teacher * const history_teacher) {
+    historyTeacher = history_teacher;
+}
+
+Teacher * Class::get_biology_teacher() const {
+    return biologyTeacher;
+}
+
+void Class::set_biology_teacher(Teacher * const biology_teacher) {
+    biologyTeacher = biology_teacher;
+}
+
+Teacher * Class::get_physics_teacher() const {
+    return physicsTeacher;
+}
+
+void Class::set_physics_teacher(Teacher * const physics_teacher) {
+    physicsTeacher = physics_teacher;
+}
+
+Teacher * Class::get_chemistry_teacher() const {
+    return chemistryTeacher;
+}
+
+void Class::set_chemistry_teacher(Teacher * const chemistry_teacher) {
+    chemistryTeacher = chemistry_teacher;
+}
+
+Teacher * Class::get_geography_teacher() const {
+    return geographyTeacher;
+}
+
+void Class::set_geography_teacher(Teacher * const geography_teacher) {
+    geographyTeacher = geography_teacher;
+}
+
+Teacher * Class::get_computer_science_teacher() const {
+    return computerScienceTeacher;
+}
+
+void Class::set_computer_science_teacher(Teacher * const computer_science_teacher) {
+    computerScienceTeacher = computer_science_teacher;
+}
+
+Teacher * Class::get_physical_education_teacher() const {
+    return physicalEducationTeacher;
+}
+
+void Class::set_physical_education_teacher(Teacher * const physical_education_teacher) {
+    physicalEducationTeacher = physical_education_teacher;
+}
+
+Teacher * Class::get_super_vising_teacher() const {
+    return superVisingTeacher;
+}
+
+void Class::set_super_vising_teacher(Teacher * const super_vising_teacher) {
+    superVisingTeacher = super_vising_teacher;
+}
--- Class.h
+++ Class.h
@@ -9,10 +9,6 @@
 
 
 class Class {
-public:
-
-int get_class_id() const;
-
 private:
     int classId;
     static int classCount;
@@ -29,6 +25,103 @@ private:
     Teacher *superVisingTeacher;
     Student students[25];
 
+public:
+    Class(const int class_id, Teacher * const match_teacher, Teacher * const english_teacher,
+    Teacher * const polish_teacher, Teacher * const history_teacher, Teacher * const biology_teacher,
+        Teacher * const physics_teacher, Teacher * const chemistry_teacher, Teacher * const geography_teacher,
+        Teacher * const computer_science_teacher, Teacher * const physical_education_teacher,
+        Teacher * const super_vising_teacher);
+
+    int get_class_id() const {
+        return classId;
+    }
+ Teacher * get_match_teacher() const {
+        return matchTeacher;
+    }
+
+    void set_match_teacher(Teacher * const match_teacher) {
+        matchTeacher = match_teacher;
+    }
+
+    Teacher * get_english_teacher() const {
+        return englishTeacher;
+    }
+
+    void set_english_teacher(Teacher * const english_teacher) {
+        englishTeacher = english_teacher;
+    }
+
+    Teacher * get_polish_teacher() const {
+        return polishTeacher;
+    }
+
+    void set_polish_teacher(Teacher * const polish_teacher) {
+        polishTeacher = polish_teacher;
+    }
+
+    Teacher * get_history_teacher() const {
+        return historyTeacher;
+    }
+
+    void set_history_teacher(Teacher * const history_teacher) {
+        historyTeacher = history_teacher;
+    }
+
+    Teacher * get_biology_teacher() const {
+        return biologyTeacher;
+    }
+
+    void set_biology_teacher(Teacher * const biology_teacher) {
+        biologyTeacher = biology_teacher;
+    }
+
+    Teacher * get_physics_teacher() const {
+        return physicsTeacher;
+    }
+
+    void set_physics_teacher(Teacher * const physics_teacher) {
+        physicsTeacher = physics_teacher;
+    }
+
+    Teacher * get_chemistry_teacher() const {
+        return chemistryTeacher;
+    }
+
+    void set_chemistry_teacher(Teacher * const chemistry_teacher) {
+        chemistryTeacher = chemistry_teacher;
+    }
+
+    Teacher * get_geography_teacher() const {
+        return geographyTeacher;
+    }
+
+    void set_geography_teacher(Teacher * const geography_teacher) {
+        geographyTeacher = geography_teacher;
+    }
+
+    Teacher * get_computer_science_teacher() const {
+        return computerScienceTeacher;
+    }
+
+    void set_computer_science_teacher(Teacher * const computer_science_teacher) {
+        computerScienceTeacher = computer_science_teacher;
+    }
+
+    Teacher * get_physical_education_teacher() const {
+        return physicalEducationTeacher;
+    }
+
+    void set_physical_education_teacher(Teacher * const physical_education_teacher) {
+        physicalEducationTeacher = physical_education_teacher;
+    }
+
+    Teacher * get_super_vising_teacher() const {
+        return superVisingTeacher;
+    }
+
+    void set_super_vising_teacher(Teacher * const super_vising_teacher) {
+        superVisingTeacher = super_vising_teacher;
+    }
 };
 
 
