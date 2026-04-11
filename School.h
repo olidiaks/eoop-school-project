77463e0459c7e43f7ba00ac47b793c55859109e4
Oliwier Olszewski <oliwierolszewski@proton.me>
2026-04-11 23:19:58 +0200

    Extend `School` class with subject-specific teacher lists and list of classes

--- School.h
+++ School.h
@@ -4,9 +4,27 @@
 
 #ifndef PROJECT_SCHOOL_H
 #define PROJECT_SCHOOL_H
+#include <list>
+
+#include "Class.h"
+#include "Teacher.h"
 
 
 class School {
+private:
+    std::list<Teacher> mathTeachers;
+    std::list<Teacher> englishTeachers;
+    std::list<Teacher> polishTeachers;
+    std::list<Teacher> historyTeachers;
+    std::list<Teacher> biologyTeachers;
+    std::list<Teacher> physicsTeachers;
+    std::list<Teacher> chemistryTeachers;
+    std::list<Teacher> geographyTeachers;
+    std::list<Teacher> computerScienceTeachers;
+    std::list<Teacher> physicalEducationTeachers;
+    std::list<Teacher> superVisingTeachers;
+    std::list<Class> classes;
+
 };
 
 
