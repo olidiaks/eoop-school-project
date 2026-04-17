5c2990a43219521bfaee565239168af07805b723
Oliwier Olszewski <oliwierolszewski@proton.me>
2026-04-17 23:56:40 +0200

    Add subject-specific grade calculation methods to `Student` class

--- Student.cpp
+++ Student.cpp
@@ -10,6 +10,19 @@ Student::Student(const std::string &first_name, const std::string &last_name, co
 ) : Person(first_name, last_name, email, day, month, year) {
 }
 
+
+float Student::get_average_grades_from_subject(const std::list<Assigment> &assignments) {
+    float sum = 0;
+    int count = 0;
+
+    for (auto &assignment : assignments) {
+        sum += assignment.get_grade();
+        count++;
+    }
+
+    return sum / count;
+}
+
 Student::Student(const std::string &first_name, const std::string &last_name, const std::string &email,
                  const time_t birth_date
 ) : Person(first_name, last_name, email, birth_date) {
@@ -18,3 +31,35 @@ Student::Student(const std::string &first_name, const std::string &last_name, co
 float Student::get_average_grade() const {
     throw std::runtime_error("Not implemented");
 }
+
+float Student::get_average_grade_from_math() const {
+    return get_average_;
+}
+
+float Student::get_average_grade_from_english() const {
+}
+
+float Student::get_average_grade_from_polish() const {
+}
+
+float Student::get_average_grade_from_history() const {
+}
+
+float Student::get_average_grade_from_biology() const {
+}
+
+float Student::get_average_grade_from_phisics() const {
+}
+
+float Student::get_average_grade_from_chemistry() const {
+}
+
+float Student::get_average_grade_from_geography() const {
+}
+
+float Student::get_average_grade_from_computer_science() const {
+}
+
+float Student::get_average_grade_from_physical_education() const {
+}
+
--- Student.h
+++ Student.h
@@ -14,25 +14,47 @@
 
 class Student : public Person {
 private:
-        std::list<Assigment> mathAssignments;
-        std::list<Assigment> englishAssignments;
-        std::list<Assigment> polishAssignments;
-        std::list<Assigment> historyAssignments;
-        std::list<Assigment> biologyAssignments;
-        std::list<Assigment> physicsAssignments;
-        std::list<Assigment> chemistryAssignments;
-        std::list<Assigment> geographyAssignments;
-        std::list<Assigment> computerScienceAssignments;
-        std::list<Assigment> physicalEducationAssignments;
+    std::list<Assigment> mathAssignments;
+    std::list<Assigment> englishAssignments;
+    std::list<Assigment> polishAssignments;
+    std::list<Assigment> historyAssignments;
+    std::list<Assigment> biologyAssignments;
+    std::list<Assigment> physicsAssignments;
+    std::list<Assigment> chemistryAssignments;
+    std::list<Assigment> geographyAssignments;
+    std::list<Assigment> computerScienceAssignments;
+    std::list<Assigment> physicalEducationAssignments;
 
-    public:
+    [[nodiscard]] static float get_average_grades_from_subject(const std::list<Assigment> &assignments);
+
+public:
     Student(const std::string &first_name, const std::string &last_name, const std::string &email,
             const time_t birth_date);
 
     Student(const std::string &first_name, const std::string &last_name, const std::string &email,
             const int &day, const int &month, const int &year);
 
-        [[nodiscard]] float get_average_grade() const;
+    [[nodiscard]] float get_average_grade() const;
+
+    [[nondiscard]] float get_average_grade_from_math() const;
+
+    [[nondiscard]] float get_average_grade_from_english() const;
+
+    [[nondiscard]] float get_average_grade_from_polish() const;
+
+    [[nondiscard]] float get_average_grade_from_history() const;
+
+    [[nondiscard]] float get_average_grade_from_biology() const;
+
+    [[nondiscard]] float get_average_grade_from_phisics() const;
+
+    [[nondiscard]] float get_average_grade_from_chemistry() const;
+
+    [[nondiscard]] float get_average_grade_from_geography() const;
+
+    [[nondiscard]] float get_average_grade_from_computer_science() const;
+
+    [[nondiscard]] float get_average_grade_from_physical_education() const;
 };
 
 #endif //PROJECT_STUDENT_H
