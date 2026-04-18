7dc4af1a75ac4d989e92bbc79af42b0ec005f594
Oliwier Olszewski <oliwierolszewski@proton.me>
2026-04-18 14:43:47 +0200

    Update unit tests to remove references to `grade` field in `Assigment` class after class simplification

--- Assigment.test.cpp
+++ Assigment.test.cpp
@@ -12,16 +12,14 @@ TEST(AssigmentTest, ConstructorWithoutGrade) {
     EXPECT_EQ(a.get_name(), "Test Name");
     EXPECT_EQ(a.get_description(), "Test Description");
     EXPECT_EQ(a.get_subject(), "Test Subject");
-    EXPECT_EQ(a.get_grade(), 0);
     EXPECT_GT(a.get_id(), 0);
 }
 
 TEST(AssigmentTest, ConstructorWithGrade) {
-    Assigment a("Math HW", "Calculus problems", "Math", 5);
+    Assigment a("Math HW", "Calculus problems", "Math");
     EXPECT_EQ(a.get_name(), "Math HW");
     EXPECT_EQ(a.get_description(), "Calculus problems");
     EXPECT_EQ(a.get_subject(), "Math");
-    EXPECT_EQ(a.get_grade(), 5);
 }
 
 // Test setters and getters
@@ -29,54 +27,49 @@ TEST(AssigmentTest, SettersAndGetters) {
     Assigment a("Name", "Desc", "Subj");
     a.set_name("New Name");
     a.set_description("New Desc");
-    a.set_grade(4);
-    
+
     EXPECT_EQ(a.get_name(), "New Name");
     EXPECT_EQ(a.get_description(), "New Desc");
-    EXPECT_EQ(a.get_grade(), 4);
 }
 
 // Test copy constructor
 TEST(AssigmentTest, CopyConstructor) {
-    Assigment a1("Name", "Desc", "Subj", 3);
+    Assigment a1("Name", "Desc", "Subj");
     Assigment a2(a1);
     
     EXPECT_EQ(a1.get_id(), a2.get_id());
     EXPECT_EQ(a1.get_name(), a2.get_name());
     EXPECT_EQ(a1.get_description(), a2.get_description());
     EXPECT_EQ(a1.get_subject(), a2.get_subject());
-    EXPECT_EQ(a1.get_grade(), a2.get_grade());
     EXPECT_EQ(a1, a2);
 }
 
 // Test move constructor
 TEST(AssigmentTest, MoveConstructor) {
-    Assigment a1("Name", "Desc", "Subj", 3);
+    Assigment a1("Name", "Desc", "Subj");
     int original_id = a1.get_id();
     Assigment a2(std::move(a1));
     
     EXPECT_EQ(a2.get_id(), original_id);
     EXPECT_EQ(a2.get_name(), "Name");
-    EXPECT_EQ(a2.get_grade(), 3);
 }
 
 // Test copy assignment operator
 TEST(AssigmentTest, CopyAssignment) {
-    Assigment a1("Name1", "Desc1", "Subj1", 1);
-    Assigment a2("Name2", "Desc2", "Subj2", 2);
+    Assigment a1("Name1", "Desc1", "Subj1");
+    Assigment a2("Name2", "Desc2", "Subj2");
     a2 = a1;
     
     EXPECT_EQ(a1.get_id(), a2.get_id());
     EXPECT_EQ(a1.get_name(), a2.get_name());
-    EXPECT_EQ(a1.get_grade(), a2.get_grade());
     EXPECT_EQ(a1, a2);
 }
 
 // Test move assignment operator
 TEST(AssigmentTest, MoveAssignment) {
-    Assigment a1("Name1", "Desc1", "Subj1", 1);
+    Assigment a1("Name1", "Desc1", "Subj1");
     int original_id = a1.get_id();
-    Assigment a2("Name2", "Desc2", "Subj2", 2);
+    Assigment a2("Name2", "Desc2", "Subj2");
     a2 = std::move(a1);
     
     EXPECT_EQ(a2.get_id(), original_id);
@@ -85,8 +78,8 @@ TEST(AssigmentTest, MoveAssignment) {
 
 // Test equality and inequality operators
 TEST(AssigmentTest, EqualityOperators) {
-    Assigment a1("Name", "Desc", "Subj", 5);
-    Assigment a2("Name", "Desc", "Subj", 5);
+    Assigment a1("Name", "Desc", "Subj");
+    Assigment a2("Name", "Desc", "Subj");
     // These should have different IDs, thus they are not equal
     
     EXPECT_NE(a1, a2);
@@ -99,7 +92,7 @@ TEST(AssigmentTest, EqualityOperators) {
 
 // Test output stream operator
 TEST(AssigmentTest, StreamOperator) {
-    Assigment a("Name", "Desc", "Subj", 4);
+    Assigment a("Name", "Desc", "Subj");
     std::stringstream ss;
     ss << a;
     std::string output = ss.str();
@@ -114,8 +107,8 @@ TEST(AssigmentTest, StreamOperator) {
 
 // Test swap function
 TEST(AssigmentTest, SwapFunction) {
-    Assigment a1("Name1", "Desc1", "Subj1", 1);
-    Assigment a2("Name2", "Desc2", "Subj2", 2);
+    Assigment a1("Name1", "Desc1", "Subj1");
+    Assigment a2("Name2", "Desc2", "Subj2");
     
     Assigment a1_copy(a1);
     Assigment a2_copy(a2);
