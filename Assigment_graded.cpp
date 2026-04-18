52f3dcb1e01a62e527f44eb368630cbc762a7b15
Oliwier Olszewski <oliwierolszewski@proton.me>
2026-04-18 14:56:30 +0200

    Implement `Assigment_graded` class with constructors, accessors, and operators

--- Assigment_graded.cpp
+++ Assigment_graded.cpp
@@ -3,3 +3,76 @@
 //
 
 #include "Assigment_graded.h"
+
+int Assigment_graded::counter = 0;
+
+Assigment_graded::Assigment_graded(Assigment &assigment) : assigment(assigment) {
+    grade = 0;
+    id = ++counter;
+    is_graded = false;
+}
+
+Assigment_graded::Assigment_graded(Assigment &assigment, const int grade) : assigment(assigment),
+                                                                            grade(grade) {
+    is_graded = true;
+    id = ++counter;
+}
+
+Assigment_graded::Assigment_graded(const Assigment_graded &other) : assigment(other.assigment),
+                                                                    grade(other.grade), id(other.id),
+                                                                    is_graded(other.is_graded) {
+}
+
+Assigment_graded::Assigment_graded(Assigment_graded &&other) noexcept : assigment(other.assigment),
+                                                                        grade(other.grade), id(other.id),
+                                                                        is_graded(other.is_graded) {
+}
+
+Assigment_graded &Assigment_graded::operator=(Assigment_graded other) {
+    using std::swap;
+    swap(*this, other);
+    return *this;
+}
+
+int Assigment_graded::get_grade() const {
+    return grade;
+}
+
+void Assigment_graded::set_grade(const int grade) {
+    this->grade = grade;
+}
+
+Assigment Assigment_graded::get_assigment() const {
+    return assigment;
+}
+
+int Assigment_graded::get_id() const {
+    return id;
+}
+
+bool operator==(const Assigment_graded &lhs, const Assigment_graded &rhs) {
+    return lhs.id == rhs.id
+           && lhs.assigment == rhs.assigment
+           && lhs.grade == rhs.grade
+           && lhs.is_graded == rhs.is_graded;
+}
+
+bool operator!=(const Assigment_graded &lhs, const Assigment_graded &rhs) {
+    return !(lhs == rhs);
+}
+
+std::ostream &operator<<(std::ostream &os, const Assigment_graded &obj) {
+    return os
+           << "id: " << obj.id
+           << " is_graded: " << obj.is_graded
+           << " grade: " << obj.grade
+           << " assigment: " << obj.assigment;
+}
+
+void swap(Assigment_graded &lhs, Assigment_graded &rhs) noexcept {
+    using std::swap;
+    swap(lhs.id, rhs.id);
+    swap(lhs.assigment, rhs.assigment);
+    swap(lhs.grade, rhs.grade);
+    swap(lhs.is_graded, rhs.is_graded);
+}
--- Assigment_graded.h
+++ Assigment_graded.h
@@ -4,11 +4,45 @@
 
 #ifndef EOOP_SCHOOL_PROJECT_ASSIGEMNT_GRADED_H
 #define EOOP_SCHOOL_PROJECT_ASSIGEMNT_GRADED_H
+#include <ostream>
 
+#include "Assigment.h"
 
 
 class Assigment_graded {
+private:
+static int counter;
+    int id;
+    Assigment &assigment;
+    int grade;
+    bool is_graded;
 
+public:
+explicit Assigment_graded(Assigment &assigment);
+
+Assigment_graded(Assigment &assigment, const int grade);
+
+Assigment_graded(const Assigment_graded &other);
+
+Assigment_graded(Assigment_graded &&other) noexcept;
+
+Assigment_graded & operator=(Assigment_graded other);
+
+[[nodiscard]] int get_grade() const;
+
+void set_grade(const int grade);
+
+[[nodiscard]] Assigment get_assigment() const;
+
+[[nodiscard]] int get_id() const;
+
+friend bool operator==(const Assigment_graded &lhs, const Assigment_graded &rhs);
+
+friend bool operator!=(const Assigment_graded &lhs, const Assigment_graded &rhs);
+
+friend std::ostream & operator<<(std::ostream &os, const Assigment_graded &obj);
+
+friend void swap(Assigment_graded &lhs, Assigment_graded &rhs) noexcept;
 };
 
 
