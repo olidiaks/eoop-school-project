5834da087b405c6d67052a9c62435f49ec2d5fcd
Oliwier Olszewski <oliwierolszewski@proton.me>
2026-04-18 14:46:14 +0200

    Add `Assigemnt_graded` class and integrate into project build system

---
+++ Assigemnt_graded.cpp
@@ -0,0 +1,5 @@
+//
+// Created by olidiaks on 4/18/26.
+//
+
+#include "Assigemnt_graded.h"
---
+++ Assigemnt_graded.h
@@ -0,0 +1,16 @@
+//
+// Created by olidiaks on 4/18/26.
+//
+
+#ifndef EOOP_SCHOOL_PROJECT_ASSIGEMNT_GRADED_H
+#define EOOP_SCHOOL_PROJECT_ASSIGEMNT_GRADED_H
+
+
+
+class Assigemnt_graded {
+
+};
+
+
+
+#endif //EOOP_SCHOOL_PROJECT_ASSIGEMNT_GRADED_H
